03b95b2e11b77e120bd6158258e345b51dd3f8d4
yshashanky <812shanky@gmail.com>
2022-07-12 23:39:06 +0530

    update

--- solution/application/my-app/src/pages/home.jsx
+++ solution/application/my-app/src/pages/home.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function home() {
-  return <div>Home</div>;
+  return (
+    <div className="image2">
+      <div className="container">
+        <div className="body">
+          <div className="introduction">
+            <div className="introductioncontainer">
+              <div className="first">SO, YOU WANT TO TRAVEL TO</div>
+              <div className="second">SPACE</div>
+              <div className="third">
+                Let’s face it; if you want to go to space, you might as well
+                genuinely go to outer space and not hover kind of on the edge of
+                it. Well sit back, and relax because we’ll give you a truly out
+                of this world experience!
+              </div>
+            </div>
+          </div>
+          <div className="hovereffect">
+            <div className="explorebackground">
+              <div className="explore">EXPLORE</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default home;
