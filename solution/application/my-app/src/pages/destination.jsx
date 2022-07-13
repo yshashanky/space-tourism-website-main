ab99eae031c196341668b871d114139fe92262db
yshashanky <812shanky@gmail.com>
2022-07-14 01:06:50 +0530

    update

--- solution/application/my-app/src/pages/destination.jsx
+++ solution/application/my-app/src/pages/destination.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import icon from "../assets/shared/logo.svg";
+import data from "../data.json";
+import moon from "../assets/destination/image-moon.png";
 
 function Destination() {
+  const destinationData = data.destinations;
+  console.log(destinationData[0]);
+
   return (
     <div className="image destinationimage">
       <div className="container">
@@ -39,21 +44,40 @@ function Destination() {
           </div>
         </div>
         <div className="body">
-          <div className="introduction">
-            <div className="introductioncontainer">
-              <div className="first">SO, YOU WANT TO TRAVEL TO</div>
-              <div className="second">SPACE</div>
-              <div className="third">
-                Let’s face it; if you want to go to space, you might as well
-                genuinely go to outer space and not hover kind of on the edge of
-                it. Well sit back, and relax because we’ll give you a truly out
-                of this world experience!
-              </div>
+          <div className="heading">
+            <span className="count">01</span>Pick your destination
+            <div className="destinationImage">
+              <img src={moon} className="" alt="moon" />
             </div>
           </div>
-          <div className="hovereffect">
-            <div className="explorebackground">
-              <div className="explore">EXPLORE</div>
+
+          <div className="destinationsDiscription">
+            <div className="destinationSelector">
+              <ul>
+                <li>Moon</li>
+                <li>Mars</li>
+                <li>Europa</li>
+                <li>Titan</li>
+              </ul>
+            </div>
+            <div className="destinationDiscription">
+              <div className="destinationName">{destinationData[0].name}</div>
+              <div className="aboutdestination">
+                {destinationData[0].description}
+              </div>
+              <div className="destinationDiscriptionFooter">
+                <div className="emptyColumn"></div>
+                <div className="discriptionFooter">
+                  <span className="discriptionFootertHead">Avg. Distance </span>
+                  {destinationData[0].distance}
+                </div>
+                <div className="discriptionFooter">
+                  <span className="discriptionFootertHead">
+                    Est. Travel Time{" "}
+                  </span>
+                  {destinationData[0].travel}
+                </div>
+              </div>
             </div>
           </div>
         </div>
